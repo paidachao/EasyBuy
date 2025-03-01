c2939233ee142ea9ee313ea14a47c575127f0bc1
siminion <w290165@gmail.com>
2025-03-01 09:54:00 +0800

    Changes

--- src/main/java/com/kgc/service/news/NewsService.java
+++ src/main/java/com/kgc/service/news/NewsService.java
@@ -1,4 +1,11 @@
 package com.kgc.service.news;
 
+import com.kgc.pojo.ResponseMsg;
+
 public interface NewsService {
+
+    /*
+        根据创建时间降序展示前5条新闻数据
+         */
+    ResponseMsg getFiveNewsOrderByCreateDate();
 }
--- src/test/java/com/kgc/EasyBuyApplicationTests.java
+++ src/test/java/com/kgc/EasyBuyApplicationTests.java
@@ -1,13 +1,13 @@
-package com.kgc;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class EasyBuyApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package com.kgc;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class EasyBuyApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
